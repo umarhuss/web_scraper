231ab739b28fa70f942d24ef39153cf6fcb59a96
uhuss <umar.huss25@gmail.com>
2024-04-13 15:15:11 +0100

    csv file

--- webscrapper_rightmove.py
+++ webscrapper_rightmove.py
@@ -1,5 +1,6 @@
 # Request libraies needed. 
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
 # Headers to mimic a browser request.
@@ -7,8 +8,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
 
-property_url = 'https://www.rightmove.co.uk/property-to-rent/find.html?searchType=RENT&locationIdentifier=REGION%5E87490&insId=1&radius=0.0&minPrice=&maxPrice=&minBedrooms=3&maxBedrooms=3&displayPropertyType=&maxDaysSinceAdded=&sortByPriceDescending=&_includeLetAgreed=on&primaryDisplayPropertyType=&secondaryDisplayPropertyType=&oldDisplayPropertyType=&oldPrimaryDisplayPropertyType=&letType=&letFurnishType=&houseFlatShare=#prop145942733'
-
+property_url = 'https://www.rightmove.co.uk/property-to-rent/find.html?searchType=RENT&locationIdentifier=REGION%5E87490&insId=1&radius=0.0&minPrice=&maxPrice=&minBedrooms=&maxBedrooms=&displayPropertyType=&maxDaysSinceAdded=&sortByPriceDescending=&_includeLetAgreed=on&primaryDisplayPropertyType=&secondaryDisplayPropertyType=&oldDisplayPropertyType=&oldPrimaryDisplayPropertyType=&letType=&letFurnishType=&houseFlatShare='
 response = requests.get(property_url, headers=headers)
 print(response.status_code)
 page_content = response.text
@@ -39,31 +39,41 @@ for description in document.find_all("h2", class_="propertyCard-title"):
 # Get links for each property
 all_links = []
 property_info = document.find_all('a', class_='propertyCard-link')
-
-for link in property_info:
-    href = link.get("href")
-    if href:
-        full_link = "https://www.rightmove.co.uk" + href
+# Expecting 25 links as 25 peroperties are on the page 
+num_properties = min(len(location_divs), 25)  
+for i in range(num_properties):
+    link = property_info[i].get("href")
+    if link:
+        full_link = "https://www.rightmove.co.uk" + link
         all_links.append(full_link)
 
+max_length = max(len(all_addresses), len(all_prices), len(all_descriptions), len(all_links))
+all_addresses += [[]] * (max_length - len(all_addresses))
+all_prices += [''] * (max_length - len(all_prices))
+all_descriptions += [''] * (max_length - len(all_descriptions))
+all_links += [''] * (max_length - len(all_links))
+
+for i in range(max_length):
+    if not all_addresses[i]:
+        all_addresses[i] = ['No address found']
+    if not all_prices[i]:
+        all_prices[i] = 'No price found'
+    if not all_descriptions[i]:
+        all_descriptions[i] = 'No description found'
+    if not all_links[i]:
+        all_links[i] = 'No link found'
 
 
     
 # Create csv file. 
+data ={
+    'Address': all_addresses,
+    'Description': all_descriptions,
+    'Price': all_prices,
+    'Links': all_links
+}
+
+data_frame = pd.DataFrame.from_dict(data)
+data_frame.to_csv(r"property_data.csv", encoding="utf-8", header="true", index = False)
 
 
-
-'''choose website:
-i have chosen rightmove to scrap
-
-we will get a list of properties. for each property we will  grab:
-- The property page url 
-
-for each property we will grab:
-
-- The name and location of the property 
-- The type 
-- number of bedrooms 
-- number of bathrooms 
-- price  
-'''
